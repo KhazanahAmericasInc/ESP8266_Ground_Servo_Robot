70dc5f94271daf2abbad046a89c68848ba49af40
CraigCrundwell <1crundwellcr@gmail.com>
2019-10-22 15:09:27 -0700

    Improved code comments

--- Arduino Server/Robot_Server/Robot_Server.ino
+++ Arduino Server/Robot_Server/Robot_Server.ino
@@ -1,32 +1,46 @@
+/*
+Craig Crundwell
+Oct 20, 2019
+This program is run on the ESP8266 and interfaces with a python client to control the robot
+*/
+
+//Libraries
 #include <ESP8266WiFi.h>
 #include <ESP8266WebServer.h>
 #include <Servo.h>
 #include <ArduinoJson.h>
 
+//Creates an server object and connets to the wifi
 ESP8266WebServer server;
-uint8_t pin_led = LED_BUILTIN;
 char* ssid = "Innovation-Science";
 char* password = "Creative=Choice+1!";
 
-//char* ssid = "KHAZANAH";
-//char* password = "w1f14khazanah!!";
-
+//Sets centre servo values
 int leftStop = 66;
 int rightStop = 71;
+
+//Creates two servo objects
 Servo servo_left;
 Servo servo_right;
+
+//These values track the connection frequency
 unsigned long lastConnectMillis;
 unsigned long connectMillis;
 
 void setup()
 {
+  //Sets pinouts for left and right servos
   servo_left.attach(D0);
   servo_right.attach(D1);
+
+  //The servos should stay still while the wifi connects
   servo_left.write(leftStop);
   servo_right.write(rightStop);
-  
-  pinMode(pin_led, OUTPUT);
+
+  //Connects to the wifi
   WiFi.begin(ssid,password);
+
+  //Starts the serial monitor and outputs the connection status
   Serial.begin(115200);
   Serial.println("");
   while(WiFi.status()!=WL_CONNECTED)
@@ -36,21 +50,27 @@ void setup()
   }
   Serial.println("");
   Serial.print("IP Address: ");
+
+  //This IP address is needed for the python client
   Serial.println(WiFi.localIP());
 
+  //Sends a hello world to the client
   server.on("/",[](){server.send(200,"text/plain","Hello World!");});
-  server.on("/toggle",toggleLED);
+  
+  //Creates a page to send servo commands to
   server.on("/Servo",setServo);
   server.begin();
 }
 
 void loop()
 {
+  //Checks for input from the client
   server.handleClient();
 
+  //tracks time since last connection
   lastConnectMillis = millis() - connectMillis;
-  
-  Serial.println(lastConnectMillis);
+
+  //Stops motors if connection is lost for over 1 second
   if (lastConnectMillis > 1000)
   {
     servo_left.write(leftStop);
@@ -58,27 +78,27 @@ void loop()
   }
 }
 
-void toggleLED()
-{
-  digitalWrite(pin_led,!digitalRead(pin_led));
-  server.send(204,"");
-}
-
 void setServo()
 {  
+  //Updates time of last connection
   connectMillis = millis();
 
-  Serial.println("servo");
-  
+  //Reads data from client
   String data_string = server.arg("plain");
   StaticJsonDocument<400> jDoc;
   DeserializationError err = deserializeJson(jDoc, data_string);
   JsonObject object = jDoc.as<JsonObject>();
   const char* left = object["left"];
   const char* right = object["right"];
+
+  //Converts data to strings
   String Left = left;
   String Right = right;
+
+  //Sets servo values
   servo_left.write(Left.toInt());
   servo_right.write(Right.toInt());
+
+  //Sends empty response
   server.send(204,"");
 }
--- Python Client/Python Client v1.py
+++ Python Client/Python Client v1.py
@@ -1,30 +1,41 @@
-import sys, os, json, time, keyboard
+#Craig Crundwell
+#Oct 20, 2019
+#This python program connects to the arduino server hosted by the servo robot
+#WASD is used on the keyboard to control the robot movement
 
+#imports
+import sys, os, json, time, keyboard
 import httplib2
 http = httplib2.Http()
 
-
+#This ip is found by running the arduno code connected to the serial monitor
 url_json = 'http://10.8.9.108/Servo'
-#url_json = 'http://172.29.32.71/Servo'   
+
+#header for json formatted data packet   
 headers={'Content-Type': 'application/json; charset=UTF-8'}
 
 while(1):
 
+    #sets command to stop
     data = {'left': '67', 'right': '71'}
 
+    #forward
     if keyboard.is_pressed('w'):
         data = {'left': '179', 'right': '0'}
 
+    #backward
     elif keyboard.is_pressed('s'):
         data = {'left': '0', 'right': '179'}
 
+    #left
     if keyboard.is_pressed('a'):
         data = {'left': '40', 'right': '40'}
 
+    #right
     if keyboard.is_pressed('d'):
         data = {'left': '90', 'right': '90'}
 
+    #sends command to server
     response, content = http.request(url_json, 'POST', headers=headers, body=json.dumps(data))
-    #print (response)
-    #print (content)
+
 
